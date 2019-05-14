f4d132cea2110806d7155c6a9f046e5edb5f9af5
Eloir Corona <personal@eloircorona.com>
2019-05-14 11:42:53 -0500

    Update Files

--- index.php
+++ index.php
@@ -21,34 +21,32 @@ if(isset($_GET['clear']))
 if(isset($_GET['image'])) $_SESSION['image'] = $_GET['image'];
 if(isset($_GET['opt'])) $_SESSION['opt'] = $_GET['opt'];
 
-$opt_names = array(
-    '0' => 'Separar color',
-    '1' => 'Monocromatico',
-    '2' => 'Blur',
-    '3' => 'Espejo',
-    '4' => 'Bordes',
-    '5' => 'Movimiento'
-);
+$opt_name_1 = 'Separar color';
+$opt_name_2 = 'Monocromatico';
+$opt_name_3 = 'Blur';
+$opt_name_4 = 'Espejo';
+$opt_name_5 = 'Bordes';
+$opt_name_6 = 'Movimiento';
 
 $opt_name = '';
 switch ($_SESSION['opt']) {
     case 1:
-        $opt_name = $opt_names[0];
+        $opt_name = $opt_name_1;
         break;
     case 2:
-        $opt_name = $opt_names[1];
+        $opt_name = $opt_name_2;
         break;
     case 3:
-        $opt_name = $opt_names[2];
+        $opt_name = $opt_name_3;
         break;
     case 4:
-        $opt_name = $opt_names[3];
+        $opt_name = $opt_name_4;
         break;
     case 5:
-        $opt_name = $opt_names[4];
+        $opt_name = $opt_name_5;
         break;
     case 6:
-        $opt_name = $opt_names[5];
+        $opt_name = $opt_name_6;
         break;
 
     default:
@@ -63,7 +61,12 @@ $data['menu'] = array(
     'image' => $_SESSION['image'],
     'image_name' => $BASE_URL . 'assets/uploads/' . $_SESSION['image'],
     'opt_name' => $opt_name,
-    'opt_names' => $opt_names,
+    'opt_name_1' => $opt_name_1,
+    'opt_name_2' => $opt_name_2,
+    'opt_name_3' => $opt_name_3,
+    'opt_name_4' => $opt_name_4,
+    'opt_name_5' => $opt_name_5,
+    'opt_name_6' => $opt_name_6,
     'image_processed' => $BASE_URL . 'assets/processed/' . $_SESSION['image']
 );
 
@@ -75,18 +78,23 @@ if(isset($_POST['submit']))
     $_SESSION['image'] = $_FILES["file"]["name"];
 
     $path = 'assets/uploads/' . basename($_FILES["file"]["name"]);
+    $_SESSION['path'] = $path;
+
     $imageFileType = strtolower(pathinfo($path, PATHINFO_EXTENSION));
     if (file_exists($path)) $upload = 0;
     if($imageFileType != "bmp") $upload = 2;
 
     if($upload == 0)
     {
-        $_SESSION['output'] = "Ya existe el archivo";
+        $_SESSION['output'] = "Ya existe el archivo: " . $path;
+    } else {
+            $upload = 1;
     }
-    elseif($upload == 1)
+
+    if($upload == 1)
     {
         if (move_uploaded_file($_FILES["file"]["tmp_name"], $path)) {
-            $_SESSION['output'] = 'Archivo subido exitosamente, ' . execute($path, 1, 255, 0, 0);
+            $_SESSION['output'] = 'Archivo subido exitosamente: ' . $path;
         } else {
             $_SESSION['output'] = 'Error al subir el archivo';
         }
@@ -96,10 +104,17 @@ if(isset($_POST['submit']))
         $_SESSION['output'] = "El archivo no está en formato BMP";
     }
     $data['menu']['image_processed'] = $BASE_URL . 'assets/processed/' . $_SESSION['image'];
-} else $_SESSION['output'] = 'Favor de subir una imagen';
+}
+
+if(isset($_SESSION['path']))
+{
+    execute($_SESSION['path'], $_SESSION['opt'], 255, 0, 0);
+} else {
+    $_SESSION['output'] = 'Favor de subir una imagen';
+}
 $data['menu']['output'] = $_SESSION['output'];
 
-if($upload == 1) header('Location: ' . $BASE_URL . 'index.php?opt=1&image=' . $data['menu']['image']);
+if($upload == 1) header('Location: ' . $BASE_URL . 'index.php?opt=1&image=' . $_SESSION['image']);
 
 // Render template
 echo $twig->render('menu.twig', $data['menu']);
