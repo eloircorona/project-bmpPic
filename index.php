93ea8dbd3274b2e04453f3e75b054988c46d5049
Eloir Corona <personal@eloircorona.com>
2019-05-13 23:58:49 -0500

    Update Files

--- index.php
+++ index.php
@@ -9,18 +9,111 @@ echo $twig->render('header.twig');
 
 $BASE_URL = "http://localhost/project-bmpPic/";
 
+if(!isset($_SESSION['image'])) $_SESSION['image'] = '';
+if(!isset($_SESSION['opt'])) $_SESSION['opt'] = 0;
+
+if(isset($_GET['clear']))
+{
+    session_destroy();
+    header('Location: index.php');
+}
+
 if(isset($_GET['image'])) $_SESSION['image'] = $_GET['image'];
 if(isset($_GET['opt'])) $_SESSION['opt'] = $_GET['opt'];
 
+$opt_names = array(
+    '0' => 'Separar color',
+    '1' => 'Monocromatico',
+    '2' => 'Blur',
+    '3' => 'Espejo',
+    '4' => 'Bordes',
+    '5' => 'Movimiento'
+);
+
+$opt_name = '';
+switch ($_SESSION['opt']) {
+    case 1:
+        $opt_name = $opt_names[0];
+        break;
+    case 2:
+        $opt_name = $opt_names[1];
+        break;
+    case 3:
+        $opt_name = $opt_names[2];
+        break;
+    case 4:
+        $opt_name = $opt_names[3];
+        break;
+    case 5:
+        $opt_name = $opt_names[4];
+        break;
+    case 6:
+        $opt_name = $opt_names[5];
+        break;
+
+    default:
+        $opt_name = 'Sin filtro';
+        break;
+}
+
+// Variable for templates
 $data['menu'] = array(
     'base_url' => $BASE_URL,
     'opt' => $_SESSION['opt'],
     'image' => $_SESSION['image'],
-    'image_name' => $BASE_URL . 'assets/uploads/' . $_SESSION['image']
+    'image_name' => $BASE_URL . 'assets/uploads/' . $_SESSION['image'],
+    'opt_name' => $opt_name,
+    'opt_names' => $opt_names,
+    'image_processed' => $BASE_URL . 'assets/processed/' . $_SESSION['image']
 );
 
+
+// Upload image
+$upload = -1;
+if(isset($_POST['submit']))
+{
+    $_SESSION['image'] = $_FILES["file"]["name"];
+
+    $path = 'assets/uploads/' . basename($_FILES["file"]["name"]);
+    $imageFileType = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+    if (file_exists($path)) $upload = 0;
+    if($imageFileType != "bmp") $upload = 2;
+
+    if($upload == 0)
+    {
+        $_SESSION['output'] = "Ya existe el archivo";
+    }
+    elseif($upload == 1)
+    {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $path)) {
+            $_SESSION['output'] = 'Archivo subido exitosamente, ' . execute($path, 1, 255, 0, 0);
+        } else {
+            $_SESSION['output'] = 'Error al subir el archivo';
+        }
+    }
+    elseif($upload == 2)
+    {
+        $_SESSION['output'] = "El archivo no está en formato BMP";
+    }
+    $data['menu']['image_processed'] = $BASE_URL . 'assets/processed/' . $_SESSION['image'];
+} else $_SESSION['output'] = 'Favor de subir una imagen';
+$data['menu']['output'] = $_SESSION['output'];
+
+if($upload == 1) header('Location: ' . $BASE_URL . 'index.php?opt=1&image=' . $data['menu']['image']);
+
+// Render template
 echo $twig->render('menu.twig', $data['menu']);
 
-
+//Render template
 echo $twig->render('footer.twig');
+
+// Function to run C program
+function execute($path, $option, $parameter1, $parameter2, $parameter3)
+{
+    $command = 'project.exe ' . $option . ' ' . '"' . $path . '" ' . $parameter1 . ' ' . $parameter2 . ' ' . $parameter3;
+    $result = shell_exec($command);
+    return $result;
+}
+
+
 ?>
--- project.c
+++ project.c
@@ -6,11 +6,13 @@
 #include <stdlib.h>
 
 // Define
-#define MAX_WIDTH 500
-#define MAX_HEIGHT 500
+#define MAX_WIDTH 1000
+#define MAX_HEIGHT 1000
 
-#define FILTER_WIDTH 3
-#define FILTER_HEIGHT 3
+#define FILTER_WIDTH 5
+#define FILTER_HEIGHT 5
+
+#define MAX_LENGTH 100
 
 // Structs
 typedef struct fileHeader {
@@ -36,6 +38,7 @@ typedef struct fileInfoHeader {
 } fileInfoHeader;
 
 typedef struct fileData {
+    char filename[MAX_LENGTH];
     char dataArray[MAX_WIDTH][MAX_HEIGHT][3];
     char newArray[MAX_HEIGHT][MAX_WIDTH][3];
 } fileData;
@@ -45,44 +48,59 @@ fileInfoHeader fInfoHeader;
 fileData fdata;
 
 //global variables
-double filter[FILTER_WIDTH][FILTER_HEIGHT] = { 0,0,0,
-                                               0,1,0,
-                                               0,0,0};
-double factor = 0.0;
+double filter[FILTER_WIDTH][FILTER_HEIGHT] = {1,1,1,1,1,
+                                              1,1,1,1,1,
+                                              1,1,1,1,1,
+                                              1,1,1,1,1,
+                                              1,1,1,1,1};
+double factor = 25.0;
 double bias = 0.0;
 
 // Function Declarations
-int readFile(char *filename);
+int readFile();
 char interface();
 void printPicture();
 void colorSeparation(int input[]);
 void blur();
+void movimiento();
+void matrices(double filter[][5], double factor, double bias);
 void createFile();
+void bordes();
+void monocromatico();
+void espejo();
 
 // Functions
 int main(int agrc, char *agrv[])
 {
+    setbuf(stdout, NULL);
+
     int option = atoi(agrv[1]);
-    if(readFile(agrv[2]))
+    strcpy(fdata.filename, agrv[2]);
+    if(readFile(fdata.filename))
     {
         int input[3];
         input[0] = atoi(agrv[3]);
         input[1] = atoi(agrv[4]);
         input[2] = atoi(agrv[5]);
 
-        colorSeparation(input);
-        printPicture();
-
-    } else ;
+        switch (option) {
+            case 1: printf("1\n"); colorSeparation(input); break;
+            case 2: printf("2\n"); monocromatico(); break;
+            case 3: printf("3\n"); blur(); break;
+            case 4: printf("4\n"); espejo(); break;
+            case 5: printf("5\n"); bordes(); break;
+            case 6: printf("6\n"); movimiento(); break;
+        }
+    }
     createFile();
 }
 
-int readFile(char filename[])
+int readFile()
 {
-    printf("Nombre de Archivo: %s\n", filename);
+    printf("Nombre de Archivo: %s\n", fdata.filename);
 
     FILE *file;
-    file = fopen(filename, "rb");
+    file = fopen(fdata.filename, "rb");
 
     if(!file) printf("No se puede cargar la imagen\n");
     else
@@ -155,19 +173,19 @@ void colorSeparation(int input[])
                          fdata.newArray[i][j][2] = input_b;
 
                     } else {
-                        fdata.newArray[i][j][0] = 0;
-                        fdata.newArray[i][j][1] = 0;
-                        fdata.newArray[i][j][2] = 0;
+                        fdata.newArray[i][j][0] = 0xFF;
+                        fdata.newArray[i][j][1] = 0xFF;
+                        fdata.newArray[i][j][2] = 0xFF;
                     }
                 } else {
-                    fdata.newArray[i][j][0] = 0;
-                    fdata.newArray[i][j][1] = 0;
-                    fdata.newArray[i][j][2] = 0;
+                    fdata.newArray[i][j][0] = 0xFF;
+                    fdata.newArray[i][j][1] = 0xFF;
+                    fdata.newArray[i][j][2] = 0xFF;
                 }
             } else {
-                fdata.newArray[i][j][0] = 0;
-                fdata.newArray[i][j][1] = 0;
-                fdata.newArray[i][j][2] = 0;
+                fdata.newArray[i][j][0] = 0xFF;
+                fdata.newArray[i][j][1] = 0xFF;
+                fdata.newArray[i][j][2] = 0xFF;
             }
         }
     }
@@ -212,11 +230,20 @@ void printPicture()
     }
 }
 
-
 void createFile()
 {
+    char path[MAX_LENGTH] = "assets/processed/";
+    char name[MAX_LENGTH];
+
+    for(int i = 15; i <= strlen(fdata.filename); i++)
+    {
+        name[i - 15] = fdata.filename[i];
+    }
+    strcat(path, name);
+
+    printf("%s\n", path);
     FILE *file;
-    file = fopen("imagen.bmp","w");
+    file = fopen(path,"w");
 
     fHeader.offset = sizeof(fHeader) + sizeof(fInfoHeader) - 2;
 
@@ -238,4 +265,114 @@ void createFile()
     }
 
     fclose(file);
+}
+
+void matrices(double filter[][5], double factor, double bias)
+{
+    for(int i = 0; i < fInfoHeader.height - 1; i++)
+        for(int j = 0; j < fInfoHeader.width - 1; j++)
+        {
+            double RGB[3] = {0,0,0};
+
+            for(int filX = 0; filX < FILTER_HEIGHT; filX++)
+                for(int filY = 0; filY < FILTER_WIDTH; filY++)
+                {
+                        RGB[0] += fdata.dataArray[i - FILTER_WIDTH / 2 + filX][j - FILTER_HEIGHT / 2 + filY][0] * filter[filX][filY];
+                        RGB[1] += fdata.dataArray[i - FILTER_WIDTH / 2 + filX][j - FILTER_HEIGHT / 2 + filY][1] * filter[filX][filY];
+                        RGB[2] += fdata.dataArray[i - FILTER_WIDTH / 2 + filX][j - FILTER_HEIGHT / 2 + filY][2] * filter[filX][filY];
+
+                }
+
+            RGB[0] = RGB[0] / factor + bias;
+            if(RGB[0] > 255)
+                RGB[0] = 255;
+            else if(RGB[0] < 0)
+                RGB[0] = 0;
+
+            RGB[1] = RGB[1] / factor + bias;
+            if(RGB[1] > 255)
+                RGB[1] = 255;
+            else if(RGB[1] < 0)
+               RGB[1] = 0;
+
+
+            RGB[2] = RGB[2] / factor + bias;
+            if(RGB[2] > 255)
+                RGB[2] = 255;
+            else if(RGB[2] < 0)
+                RGB[2] = 0;
+
+            fdata.newArray[i][j][0] = RGB[0];
+            fdata.newArray[i][j][1] = RGB[1];
+            fdata.newArray[i][j][2] = RGB[2];
+        }
+}
+
+void blur()
+{
+    double filter[FILTER_WIDTH][FILTER_HEIGHT] = {  1,1,1,1,1,
+                                                    1,1,1,1,1,
+                                                    1,1,1,1,1,
+                                                    1,1,1,1,1,
+                                                    1,1,1,1,1};
+    double factor = 25.0;
+    double bias = 0.0;
+
+    matrices(filter, factor, bias);
+}
+
+void movimiento()
+{
+    double filter2[FILTER_WIDTH][FILTER_HEIGHT] = { 1,0,0,0,0,
+                                                    0,1,0,0,0,
+                                                    0,0,1,0,0,
+                                                    0,0,0,1,0,
+                                                    0,0,0,0,1};
+    double factor2 = 5.0;
+    double bias2 = 0.0;
+
+    matrices(filter2, factor2, bias2);
+}
+
+void bordes()
+{
+    double filter[FILTER_WIDTH][FILTER_HEIGHT] = {  0,0,-1,0,0,
+                                                    0,0,-1,0,0,
+                                                    -1,-1,8,-1,-1,
+                                                    0,0,-1,0,0,
+                                                    0,0,-1,0,0};
+    double factor = 1.0;
+    double bias = 0.0;
+
+    matrices(filter, factor, bias);
+}
+
+void monocromatico()
+{
+    int avg;
+
+    for(int i = 0; i < fInfoHeader.height - 1; i++)
+        for(int j = 0; j < fInfoHeader.width - 1; j++)
+        {
+            for(int k = 0; k < 3; k++)
+                avg += fdata.dataArray[i][j][k];
+            avg = avg / 3;
+
+            for(int k = 0; k < 3; k++)
+                fdata.newArray[i][j][k] = avg;
+            avg = 0;
+        }
+}
+
+void espejo()
+{
+    int aux;
+
+     for(int i = 0; i < fInfoHeader.height - 1; i++)
+        for(int j = fInfoHeader.width - 1; j > 0; j--)
+            for(int k = 0; k < 3; k++)
+            {
+                aux = fdata.dataArray[i][j][k];
+                fdata.newArray[i][fInfoHeader.width - j][k] = aux;
+            }
 }
